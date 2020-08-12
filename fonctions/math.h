60fa015579000a3c178969c05ebda8b7e4a93e8a
Froissart Kévin <kevin.froissart69@gmail.com>
2020-08-12 17:04:56 +0200

    References, fonctions, headers et fichiers externes

---
+++ fonctions/math.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+
+#include <string>
+
+/**
+ * \brief Fonction qui ajoute 2 au nombre reçu en argument
+ * \param nombreRecu  Le nombre auquel la fonction ajoute 2
+ * \return nombreRecu + 2
+ */
+int ajouteDeux(int nombreRecu);
+/**/
+void afficherMessage(std::string message);
+
+#endif // MATH_H_INCLUDED
