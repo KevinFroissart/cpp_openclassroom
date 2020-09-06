e84d65aeca801f663affb84b20dc3d129dee0b91
Froissart Kévin <kevin.froissart69@gmail.com>
2020-09-06 14:57:39 +0200

    signaux et slots

--- QT GUI/heritage/mafenetre.cpp
+++ QT GUI/heritage/mafenetre.cpp
@@ -5,12 +5,13 @@ MaFenetre::MaFenetre() : QWidget()
     setFixedSize(300, 150);
 
     // Construction du bouton
-    m_bouton = new QPushButton("Pimp mon bouton !", this);
+    m_bouton = new QPushButton("Quitter !", this);
 
     m_bouton->setFont(QFont("Comic Sans MS", 14));
     m_bouton->setCursor(Qt::PointingHandCursor);
-    m_bouton->setIcon(QIcon("smile.png"));
     m_bouton->move(60, 50);
+    // Connexion du clic du bouton à la fermeture de l'application
+    QObject::connect(m_bouton, SIGNAL(clicked()), qApp, SLOT(quit()));
 }
 
 MaFenetre::~MaFenetre()
