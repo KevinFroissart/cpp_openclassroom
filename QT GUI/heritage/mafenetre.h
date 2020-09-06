d3808c3a39f06404d54648cc6efd001aff79a3da
Froissart Kévin <kevin.froissart69@gmail.com>
2020-09-06 15:12:30 +0200

    progress bar

--- QT GUI/heritage/mafenetre.h
+++ QT GUI/heritage/mafenetre.h
@@ -4,15 +4,17 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QProgressBar>
+#include <QSlider>
 
-class MaFenetre : public QWidget // On hérite de QWidget (IMPORTANT)
+class MaFenetre : public QWidget
 {
     public:
     MaFenetre();
-    ~MaFenetre();
 
     private:
-    QPushButton *m_bouton;
+    QProgressBar *m_bar;
+    QSlider *m_slider;
 };
 
 #endif
