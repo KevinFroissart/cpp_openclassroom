823e80e8564bcdbba028573c1d3396fdb482032c
Froissart Kévin <kevin.froissart69@gmail.com>
2020-09-06 15:22:28 +0200

    own slots & signals

--- QT GUI/heritage/mafenetre.cpp
+++ QT GUI/heritage/mafenetre.cpp
@@ -1,15 +1,24 @@
 #include "MaFenetre.h"
 
+
+void MaFenetre::changerLargeur(int largeur)
+{
+    setFixedSize(largeur, height());
+
+    if (largeur == 600)
+    {
+        emit agrandissementMax();
+    }
+}
+
 MaFenetre::MaFenetre() : QWidget()
 {
     setFixedSize(200, 100);
 
-    m_bar = new QProgressBar(this);
-    m_bar->setGeometry(10, 60, 150, 20);
-    m_bar->move(50, 20);
-
     m_slider = new QSlider(Qt::Horizontal, this);
+    m_slider->setRange(200, 600);
     m_slider->setGeometry(10, 60, 150, 20);
 
-    QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_bar, SLOT(setValue(int))) ;
+    QObject::connect(m_slider, SIGNAL(valueChanged(int)), this, SLOT(changerLargeur(int)));
+    QObject::connect(this, SIGNAL(agrandissementMax()), qApp, SLOT(quit()));
 }
