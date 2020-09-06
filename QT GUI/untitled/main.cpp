f0a41055b2f03ff8a4d0c97711ddd6a8dd2f32a8
Froissart Kévin <kevin.froissart69@gmail.com>
2020-09-06 14:20:27 +0200

    first QT GUI programm

---
+++ QT GUI/untitled/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QPushButton bouton("Salut les Zéros, la forme ?");
+    bouton.show();
+
+    return app.exec();
+}
